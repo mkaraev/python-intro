a11f7980ecfd487c875629457c9c0ede0c807382
mkaraev <maruf.karaev97@gmail.com>
2021-09-22 02:47:37 +0500

    Add lesson/homework for 4-th lesson

--- homeworks/homework4.py
+++ homeworks/homework4.py
@@ -106,7 +106,7 @@ def test_merge():
     assert extend([2, 4, 6], [1, 3, 5]) == [1, 2, 3, 4, 5, 6]
     assert extend([4, 5, 6], []) == [4, 5, 6]
     assert extend([], [1, 3, 5]) == [1, 3, 5]
-    assert extend([], [])
+    assert extend([], []) == []
     print("Great your solution works!")
 
 
